408faafd4283921fbed5bff0bffe35ac0d018d49
siddhantwaghjale <waghjalesiddhant@gmail.com>
2020-08-29 00:20:47 +0530

    First commit

---
+++ src/spotify.js
@@ -0,0 +1,27 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+const clientId = "4e2ae9f2b157420693b49fb9396f6389";
+const redirectUri = "http://localhost:3000/";
+const scopes = [
+    "user-read-currently-playing",
+    "user-read-recently-played",
+    "user-read-playback-state",
+    "user-top-read",
+    "user-modify-playback-state",
+  ];
+
+  export const getTokenFromUrl = () => {
+      return window.location.hash
+      .substring(1)
+      .split("&")
+      .reduce((initial, item) => {
+        var parts = item.split("=");
+        initial[parts[0]] = decodeURIComponent(parts[1]);
+  
+        return initial;
+      }, {});
+  }
+
+  export const accessUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+    "%20"
+  )}&response_type=token&show_dialog=true`;
